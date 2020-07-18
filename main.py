bb1f0f3c4e3d06348252ed4722d9c526bae11747
George Albuquerque Pinto <georgealbuquerquepinto@hotmail.com>
2020-07-18 11:37:01 -0300

    Add pesos de time e novos dados

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from ortools.linear_solver import pywraplp
 import json
 
 def main():
-  input_file = open ('data/2019/br2019_01.json')
-  json_array = json.load(input_file)
+  jogadores     = json.load(open('data/2019/br2019_01.json'))
+  jogadores_pos = json.load(open('data/2019/br2019_02.json'))
+  classificacao = json.load(open('data/2019/classificacao_01.json'))
+  rodada        = json.load(open('data/2019/jogos_01.json'))
 
   G = []
   L = []
@@ -12,12 +14,39 @@ def main():
   M = []
   A = []
   T = []
+  JOG = {}
+  CL = {}
+  ptsTotal = 0
 
   # Cartoletas disponíveis
-  C = 500
+  C = 114.6
+
+  # Ler dados de classificação
+  for item in classificacao:
+    time = {
+      'pos' : None,
+      'pts' : None,
+      'casa': None
+    }
+    
+    time['pos'] = int(item['pos'])
+    time['pts'] = int(item['pts'])
+    clube = item['clube']
+    ptsTotal += time['pts']
+
+    CL[clube] = time
+
+  if ptsTotal == 0:
+    ptsTotal = 1
+
+  # Ler dados de jogos
+  for item in rodada:
+    clube = item['clube']
+
+    CL[clube]['casa'] = int(item['casa'])
   
-  # Ler dados
-  for item in json_array:
+  # Ler dados de jogadores
+  for item in jogadores:
     J = {
       'apelido'   : None,
       'status_id' : None,
@@ -25,7 +54,8 @@ def main():
       'preco_num' : None,
       'media_num' : None,
       'posicao_id': None,
-      'clube'     : None
+      'clube'     : None,
+      'id'        : None
     }
     
     J['apelido']    = item['apelido']
@@ -35,6 +65,7 @@ def main():
     J['media_num']  = float(item['media_num'])
     J['posicao_id'] = int(item['posicao_id'])
     J['clube']      = item['clube']
+    J['id']         = item['atleta_id']
 
     if int(J['status_id']) == 7:
       if int(J['posicao_id']) == 1:
@@ -49,6 +80,18 @@ def main():
         A.append(J)
       elif int(J['posicao_id']) == 6:
         T.append(J)
+  
+  # Ler dados de pontuação e vaiação
+  for item in jogadores_pos:
+    J = {
+      'pontos_num'  : None,
+      'variacao_num': None
+    }
+    
+    J['pontos_num']    = float(item['pontos_num'])
+    J['variacao_num']  = float(item['variacao_num'])
+
+    JOG[item['atleta_id']] = J
 
   # Cria solver
   solver = pywraplp.Solver('simple_lp_program', pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
@@ -124,12 +167,12 @@ def main():
 
   # Função objetivo
   solver.Maximize(
-    solver.Sum(G[i]['media_num'] * Gb[i] for i in range(len(Gb))) +
-    solver.Sum(L[i]['media_num'] * Lb[i] for i in range(len(Lb))) +
-    solver.Sum(Z[i]['media_num'] * Zb[i] for i in range(len(Zb))) +
-    solver.Sum(M[i]['media_num'] * Mb[i] for i in range(len(Mb))) +
-    solver.Sum(A[i]['media_num'] * Ab[i] for i in range(len(Ab))) +
-    solver.Sum(T[i]['media_num'] * Tb[i] for i in range(len(Tb)))
+    solver.Sum((G[i]['media_num'] + (CL[G[i]['clube']]['casa']) + (CL[G[i]['clube']]['pts'] / ptsTotal) + (2.0 - (CL[G[i]['clube']]['pos']/10.0))) * Gb[i] for i in range(len(Gb))) +
+    solver.Sum((L[i]['media_num'] + (CL[L[i]['clube']]['casa']) + (CL[L[i]['clube']]['pts'] / ptsTotal) + (2.0 - (CL[L[i]['clube']]['pos']/10.0))) * Lb[i] for i in range(len(Lb))) +
+    solver.Sum((Z[i]['media_num'] + (CL[Z[i]['clube']]['casa']) + (CL[Z[i]['clube']]['pts'] / ptsTotal) + (2.0 - (CL[Z[i]['clube']]['pos']/10.0))) * Zb[i] for i in range(len(Zb))) +
+    solver.Sum((M[i]['media_num'] + (CL[M[i]['clube']]['casa']) + (CL[M[i]['clube']]['pts'] / ptsTotal) + (2.0 - (CL[M[i]['clube']]['pos']/10.0))) * Mb[i] for i in range(len(Mb))) +
+    solver.Sum((A[i]['media_num'] + (CL[A[i]['clube']]['casa']) + (CL[A[i]['clube']]['pts'] / ptsTotal) + (2.0 - (CL[A[i]['clube']]['pos']/10.0))) * Ab[i] for i in range(len(Ab))) +
+    solver.Sum((T[i]['media_num'] + (CL[T[i]['clube']]['casa']) + (CL[T[i]['clube']]['pts'] / ptsTotal) + (2.0 - (CL[T[i]['clube']]['pos']/10.0))) * Tb[i] for i in range(len(Tb)))
   )
 
   # Solver
@@ -143,7 +186,9 @@ def main():
   cont_mei = 0
   cont_ata = 0
   preco_total = 0
-  
+  variacao = 0
+  pontuacao = 0
+
   print('┌─────┬──────────────────────┬─────────────────┬───────┐')
   print('│\033[1m POS \033[0m│\033[1m %-20s \033[0m│\033[1m %-15s \033[0m│ \033[1m%s \033[0m│' % ('JOGADOR', 'TIME', 'PREÇO'))
   print('├─────┼──────────────────────┼─────────────────┼───────┤')
@@ -152,40 +197,54 @@ def main():
     if Gb[i].solution_value() > 0:
       print('│ GOL │ %-20s │ %-15s │ %5.2f │' % (G[i]['apelido'], G[i]['clube'], G[i]['preco_num']))
       preco_total += G[i]['preco_num']
+      pontuacao += JOG[G[i]['id']]['pontos_num']
+      variacao += JOG[G[i]['id']]['variacao_num']
 
   for i in range(len(L)):
     if Lb[i].solution_value() > 0:
       print('│ LAT │ %-20s │ %-15s │ %5.2f │' % (L[i]['apelido'],L[i]['clube'],L[i]['preco_num']))
       cont_def += 1
       preco_total += L[i]['preco_num']
+      pontuacao += JOG[L[i]['id']]['pontos_num']
+      variacao += JOG[L[i]['id']]['variacao_num']
 
   for i in range(len(Z)):
     if Zb[i].solution_value() > 0:
       print('│ ZAG │ %-20s │ %-15s │ %5.2f │' % (Z[i]['apelido'],Z[i]['clube'],Z[i]['preco_num']))
       cont_def += 1
       preco_total += Z[i]['preco_num']
+      pontuacao += JOG[Z[i]['id']]['pontos_num']
+      variacao += JOG[Z[i]['id']]['variacao_num']
 
   for i in range(len(M)):
     if Mb[i].solution_value() > 0:
       print('│ MEI │ %-20s │ %-15s │ %5.2f │' % (M[i]['apelido'],M[i]['clube'],M[i]['preco_num']))
       cont_mei += 1
       preco_total += M[i]['preco_num']
+      pontuacao += JOG[M[i]['id']]['pontos_num']
+      variacao += JOG[M[i]['id']]['variacao_num']
   
   for i in range(len(A)):
     if Ab[i].solution_value() > 0:
       print('│ ATA │ %-20s │ %-15s │ %5.2f │' % (A[i]['apelido'],A[i]['clube'],A[i]['preco_num']))
       cont_ata += 1
       preco_total += A[i]['preco_num']
+      pontuacao += JOG[A[i]['id']]['pontos_num']
+      variacao += JOG[A[i]['id']]['variacao_num']
   
   for i in range(len(T)):
     if Tb[i].solution_value() > 0:
       print('│ TEC │ %-20s │ %-15s │ %5.2f │' % (T[i]['apelido'],T[i]['clube'],T[i]['preco_num']))
       preco_total += T[i]['preco_num']
+      pontuacao += JOG[T[i]['id']]['pontos_num']
+      variacao += JOG[T[i]['id']]['variacao_num']
 
   print('└─────┴──────────────────────┴─────────────────┴───────┘')
 
   print('Esquema: {}-{}-{}'.format(cont_def,cont_mei,cont_ata))
   print('Preço total: %.2f' % preco_total)
+  print('Pontuação: %.2f' % pontuacao)
+  print('Variação: %.2f' % variacao)
 
 if __name__ == '__main__':
   main()
